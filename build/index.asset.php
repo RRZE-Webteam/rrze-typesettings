f6b1927f9990d7792f227dcb540806ba8cfe9c76
Benjamin Klemencic <benjamin.klemencic@fau.de>
2024-10-10 13:57:30 +0200

    1.2.3 (updated prims-php.min.js & optimized get_lang_from_settings())

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '1ffb8cb1702742186d4b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '7f28a430668867d4517a');
