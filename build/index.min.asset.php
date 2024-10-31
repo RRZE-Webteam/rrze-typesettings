f0761938c762077249fe55b6afb995788ae27441
Benjamin Klemencic <benjamin.klemencic@fau.de>
2024-10-31 12:02:11 +0100

    wip

--- build/index.min.asset.php
+++ build/index.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b4590c3789081a2c7de5');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f481793ddd13133363d1');
